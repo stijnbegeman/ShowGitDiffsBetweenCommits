b498d69eac8ca2088a36eb605840b6e57172d0b0
Stijn Begeman <stijnbegeman@gmail.com>
2017-12-02 13:18:44 +0100

    Working

---
+++ ShowGitDiffBetweenCommits/ShowGitDiff.Sdk/Git/IGitRepositoryFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShowGitDiff.Sdk.Git
+{
+    public interface IGitRepositoryFactory
+    {
+        IGitRepository CreateGitRepository(string url);
+    }
+}
